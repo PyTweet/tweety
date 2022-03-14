e224c4ef975c77e900f4450eccdde4cc0d77dd4c
TheGenocide <>
2022-03-14 01:16:49 +0000

    deleted

--- constants.py
+++
@@ -1,1 +0,0 @@
-HINTS = ("**#1 Hint:**\nBadges represent an additional infomation about the user. for Example: ✅ represent the account is verified! There's a total of 3 different badges, can you get them all?", "**#2 Hint:**\nDid you know `e!account` will display your twitter account information?", "**#3 Hint:**\n There's a secret command in my system that only my owners know!")

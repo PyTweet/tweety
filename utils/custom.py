c6f3ff08585b377ead0d7a00336c988fbae221e6
TheGenocide <>
2022-03-25 13:49:20 +0000

    chore: Create a new ClagConverter class

--- utils/custom.py
+++ utils/custom.py
@@ -1,7 +1,13 @@
-from discord.ext.commands import Group
+from discord.ext.commands import Group, flag, FlagConverter
 
 class CommandGroup(Group):
     def __init__(self, *args, **attrs):
         self.description = attrs.get("description", "No Description Provided")        
         super().__init__(*args, **attrs)
-        
+
+class Options(FlagConverter, case_insensitive=True):
+    question: str
+    option1: str
+    option2: str
+    option3: str = flag(default="question")
+    option4: str = flag(default="question")
